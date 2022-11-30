dda213f9044685865e06f09bda34969779772c55
Marina <mv.nikolaeva1507@gmail.com>
2022-11-30 20:12:46 +0300

    correct homework_4

--- homework_4/my.sql
+++ homework_4/my.sql
@@ -1,5 +1,5 @@
 -- create
-CREATE TABLE STUDENTS (
+CREATE TABLE students (
   empId INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   age TEXT NOT NULL,
@@ -7,17 +7,17 @@ CREATE TABLE STUDENTS (
 );
 
 -- insert
-INSERT INTO STUDENTS VALUES (0001, 'Анна', '23', 'Санкт-Петербург');
-INSERT INTO STUDENTS VALUES (0002, 'Сергей', '29', 'Москва');
-INSERT INTO STUDENTS VALUES (0003, 'Полина', '27', 'Курск');
-INSERT INTO STUDENTS VALUES (0004, 'Ульяна', '25', 'Москва');
-INSERT INTO STUDENTS VALUES (0005, 'Иван', '30', 'Москва');
-INSERT INTO STUDENTS VALUES (0006, 'Юлия', '32', 'Самара');
-INSERT INTO STUDENTS VALUES (0007, 'Борис', '18', 'Москва');
-INSERT INTO STUDENTS VALUES (0008, 'Дарья', '26', 'Москва');
-INSERT INTO STUDENTS VALUES (0009, 'Михаил', '33', 'Москва');
-INSERT INTO STUDENTS VALUES (0010, 'Ирина', '19', 'Нижний Новгород');
+INSERT INTO STUDENTS VALUES (0001, 'Анна', 23, 'Санкт-Петербург');
+INSERT INTO STUDENTS VALUES (0002, 'Сергей', 29, 'Москва');
+INSERT INTO STUDENTS VALUES (0003, 'Полина', 27, 'Курск');
+INSERT INTO STUDENTS VALUES (0004, 'Ульяна', 25, 'Москва');
+INSERT INTO STUDENTS VALUES (0005, 'Иван', 30, 'Москва');
+INSERT INTO STUDENTS VALUES (0006, 'Юлия', 32, 'Самара');
+INSERT INTO STUDENTS VALUES (0007, 'Борис', 18, 'Москва');
+INSERT INTO STUDENTS VALUES (0008, 'Дарья', 26, 'Москва');
+INSERT INTO STUDENTS VALUES (0009, 'Михаил', 33, 'Москва');
+INSERT INTO STUDENTS VALUES (0010, 'Ирина', 19, 'Нижний Новгород');
 
 -- fetch 
-SELECT name FROM STUDENTS WHERE age BETWEEN '18' AND '29' AND adress = 'Москва';
+SELECT name FROM STUDENTS WHERE age BETWEEN 18 AND 29 AND adress = 'Москва';
 
